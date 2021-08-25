26cb2d75df89667cd7f3a1c064f3f8bb865a66bf
Mahamat Naim Abdelkerim <naimdevelopper@gmail.com>
2021-08-25 09:44:06 -0700

    first deploy

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '6751f4836f0b4fb01fead79e09600b44'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0cb3f0e6-c1d3-4bef-8af9-28ebd0f94e3b'
+};
