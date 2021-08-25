f63ad2409b92fed84f1b8054acd586f152c8ce75
Mahamat Naim Abdelkerim <naimdevelopper@gmail.com>
2021-08-25 11:28:47 -0700

    first deploy

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,20 +1,20 @@
-const parse = require('pg-connection-string').parse;
+const parse = require("pg-connection-string").parse;
 const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'postgres',
+        client: "postgres",
         host: config.host,
         port: config.port,
         database: config.database,
         username: config.user,
         password: config.password,
         ssl: {
-          rejectUnauthorized: false,
+          rejectUnauthorized: true,
         },
       },
       options: {
